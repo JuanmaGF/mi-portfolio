13df7f0247c96baf90704b6dc39e33c4700baeb4
JuanmaGF <jmgf.1996@gmail.com>
2024-12-02 18:46:18 +0100

    Footer Arreglado

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import Habilidades from './components/Habilidades';
 import Proyectos from './components/Proyectos';
 import Contacto from './components/Contacto';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Contenedor para el contenido principal */}
+        <div className="content">
         <Navbar />
-        
-        <Routes>
-          {/* Definir cada ruta y el componente que se mostrará */}
-          <Route path="/" element={<SobreMi />} />
-          <Route path="/habilidades" element={<Habilidades />} />
-          <Route path="/proyectos" element={<Proyectos />} />
-          <Route path="/contacto" element={<Contacto />} />
-        </Routes>
-
+          <Routes>
+            <Route path="/" element={<SobreMi />} />
+            <Route path="/habilidades" element={<Habilidades />} />
+            <Route path="/proyectos" element={<Proyectos />} />
+            <Route path="/contacto" element={<Contacto />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
